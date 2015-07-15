cb07b8f448f347954b3fa03c82641da460fc0e5f
k0pernicus <antonin.carette@gmail.com>
2015-07-15 18:51:51 +0200

    libs/learning/learning_algorithms/dichotomic_approach.py : definition of the dichotomic learning algorithm added

---
+++ libs/learning/learning_algorithms/dichotomic_approach.py
@@ -0,0 +1,7 @@
+def dichotomic_learning_algorithm(graph, nb_batch):
+    """
+    Algorithm to compute the weight of each edge, with a dichotomic approach.
+    This algorithm is detailed in the scientific paper (see README.md).
+    graph : A Graph object
+    nb_batch : An integer to represent the number of iteration to learn (can accurate or not learning results)
+    """
