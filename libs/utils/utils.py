7498df3aa2d6da409bd6614aab02fcb588e71df0
k0pernicus <antonin.carette@gmail.com>
2015-07-16 11:51:45 +0200

    libs/utils/utils.py : ChunksList() added

--- libs/utils/utils.py
+++ libs/utils/utils.py
@@ -4,3 +4,14 @@ def isEmpty(list):
     """
 
     return len(list) == 0
+
+def chunksList(list, n):
+    """
+    Yield successive n-sized chunks from list
+    list: The list to chunks
+    n: The number of elements in each sublist
+    """
+    list_to_return = []
+    for i in range(0, len(list), n):
+        list_to_return += [list[i:i+n]]
+    return list_to_return
