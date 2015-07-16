7823c07780254c9b25ad556c9c65a762a91e8133
k0pernicus <antonin.carette@gmail.com>
2015-07-16 12:11:37 +0200

    libs/learning_and_testing_lib/testing/Testing.py : init function added

--- libs/learning_and_testing_lib/testing/Testing.py
+++ libs/learning_and_testing_lib/testing/Testing.py
@@ -1,5 +1,6 @@
 from libs.learning_and_testing_lib.DefaultStructure import DefaultStructure
 
-class TestingSet(DefaultStructure) :
+class TestingSet(DefaultStructure):
 
-    pass
+    def __init__(self, id, id_graph, tests, debug_mod):
+        super().__init__(id, id_graph, tests, debug_mod)
