3165dbb9732a6696529e9f4b59edc7333e006ad3
k0pernicus <antonin.carette@gmail.com>
2015-07-16 14:45:09 +0200

    libs/graph_generation/graph_generation_algorithms/BarabasiGraph.py : new field 'method'

--- libs/graph_generation/graph_generation_algorithms/BarabasiGraph.py
+++ libs/graph_generation/graph_generation_algorithms/BarabasiGraph.py
@@ -7,18 +7,19 @@ class BarabasiGraph(Graph):
         Object representing a graph, made with the Barabasi-Albert model.
     """
 
-    def __init__(self, id, nb_nodes, degree, nb_ex, debug_mod = False):
+    def __init__(self, id, nb_nodes, degree, nb_ex, method="uniform", debug_mod = False):
         """
             We call super() to initialize a basic graph with initial properties.
             id : An id to represent the graph
             nb_nodes : An integer to represent the number of nodes in the model
             degree : An integer to represent the average degree of each node in the model
             nb_ex : An integer to represent the number of examples to generate (learning and training)
+            method : A string which represents the method used to generate probabilities ("unform", "random")
             debug_mod : A boolean to know if the graph is on debugging mod or not
             Note : We add the degree field because the generation of the graph, with the Barabasi-Albert model, needs this one - it's not important for other graphs models (like the Musco model).
         """
         self.degree = degree
-        super().__init__(id, nb_nodes, nb_ex, debug_mod)
+        super().__init__(id, nb_nodes, nb_ex, method, debug_mod)
 
     def generate(self):
         """
