198714e9a948b0c9a6b22b3e687d3d111a7f692c
k0pernicus <antonin.carette@gmail.com>
2015-07-16 15:29:27 +0200

    libs/graph_generation/graph_generation_algorithms/MuscoGraph.py : acyclic and directed graph verification

--- libs/graph_generation/graph_generation_algorithms/MuscoGraph.py
+++ libs/graph_generation/graph_generation_algorithms/MuscoGraph.py
@@ -4,6 +4,8 @@ import random
 
 import networkx as nx
 
+from networkx import is_directed_acyclic_graph
+
 class MuscoGraph(Graph):
     """
         Object representing a graph, made with the Musco model.
@@ -25,27 +27,23 @@ class MuscoGraph(Graph):
             Method to generate the graph based on the Musco model.
             Return a Graph object, based on Musco model - the musco model is available in a scientific paper (see README.md).
         """
+
         new_graph_vincenzo = nx.DiGraph()
 
-        p = 0.6
+        #we return the graph (new_graph_vincenzo) if he's not acyclic and directed
+        while True:
 
-        q = 0.4
+            p = 0.6
 
-        all_nodes = [0,1,2]
+            q = 0.4
 
-        all_edges = [(0,1), (0,2)]
+            all_nodes = [0,1,2]
 
-        for i in range(3, self.nb_nodes):
-            all_nodes.append(i)
-            if random.uniform(0,1) <= p:
-                random_node = random.choice(all_nodes)
-                while random_node == i:
-                    random_node = random.choice(all_nodes)
-                all_edges.append((i, random_node))
-                children = [child for (source, child) in all_edges if (source == random_node)]
-                for child in children:
-                    all_edges.append((i, child))
-                if random.uniform(0,1) <= q:
+            all_edges = [(0,1), (0,2)]
+
+            for i in range(3, self.nb_nodes):
+                all_nodes.append(i)
+                if random.uniform(0,1) <= p:
                     random_node = random.choice(all_nodes)
                     while random_node == i:
                         random_node = random.choice(all_nodes)
@@ -53,14 +51,27 @@ class MuscoGraph(Graph):
                     children = [child for (source, child) in all_edges if (source == random_node)]
                     for child in children:
                         all_edges.append((i, child))
-            else:
-                random_node = random.choice(all_nodes)
-                while random_node == i:
+                    if random.uniform(0,1) <= q:
+                        random_node = random.choice(all_nodes)
+                        while random_node == i:
+                            random_node = random.choice(all_nodes)
+                        all_edges.append((i, random_node))
+                        children = [child for (source, child) in all_edges if (source == random_node)]
+                        for child in children:
+                            all_edges.append((i, child))
+                else:
                     random_node = random.choice(all_nodes)
-                all_edges.append((random_node, i))
+                    while random_node == i:
+                        random_node = random.choice(all_nodes)
+                    all_edges.append((random_node, i))
 
-        new_graph_vincenzo.add_nodes_from(all_nodes)
+            new_graph_vincenzo.add_nodes_from(all_nodes)
 
-        new_graph_vincenzo.add_edges_from(all_edges)
+            new_graph_vincenzo.add_edges_from(all_edges)
+
+            if is_directed_acyclic_graph(new_graph_vincenzo):
+                break
+            else:
+                new_graph_vincenzo = nx.DiGraph()
 
         return new_graph_vincenzo
