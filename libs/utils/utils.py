9d5b086b5c5fbe1e638d9d09889dd3dead4090f4
k0pernicus <antonin.carette@gmail.com>
2015-07-17 15:48:48 +0200

    libs/utils/utils.py : Function to return a list of edges by a path

--- libs/utils/utils.py
+++ libs/utils/utils.py
@@ -15,3 +15,12 @@ def chunksList(list, n):
     for i in range(0, len(list), n):
         list_to_return += [list[i:i+n]]
     return list_to_return
+
+def getExistingPathsFrom(path):
+    """
+    Function to transform the list of nodes (all_paths) to a list of tuples (source, target)
+    Ex: [node1, node2, node3, node4, ...] -> [(node1, node2),(node2, node3), (node3, node4), (node4,...)]
+    path: A path in the usegraph
+    """
+
+    return [(path[a], path[a + 1]) for a in range(0, len(path) - 1)]
