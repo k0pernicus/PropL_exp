7dcab32ea9b6e8697af58220f9a4f59a8b324f18
k0pernicus <antonin.carette@gmail.com>
2015-07-17 11:34:46 +0200

    libs/learning_and_testing_lib/learning/Learning.py : Implementation of LearningSet object

--- libs/learning_and_testing_lib/learning/Learning.py
+++ libs/learning_and_testing_lib/learning/Learning.py
@@ -1,6 +1,70 @@
 from libs.learning_and_testing_lib.DefaultStructure import DefaultStructure
 
+from libs.learning_and_testing_lib.learning.learning_algorithms.dichotomic_approach import dichotomic_approach
+# from libs.learning_and_testing_lib.learning.learning_algorithms.min_and_max_approach import min_and_max_approach
+# from libs.learning_and_testing_lib.learning.learning_algorithms.tag_each_usefull_edge_approach import tag_each_usefull_edge_approach
+# from libs.learning_and_testing_lib.learning.learning_algorithms.update_each_path_approach import update_each_path_approach
+
+import os
+
+libs_dir = "libs/"
+learning_and_testing_dir = "learning_and_testing_lib/"
+learning_dir = "learning/"
+learning_algo_dir = "learning_algorithms/"
+
+learning_algorithm_functions = {
+    'dichotomic_approach' : dichotomic_approach,
+    #'min_and_max_approach' : min_and_max_approach,
+    #'tag_each_usefull_edge_approach' : tag_each_usefull_edge_approach,
+    #'update_each_path_approach' : update_each_path_approach
+}
+
+not_usefull_files = [".DS_Store", "__pycache__", "__pycache__/", "__init__.py"]
+
 class LearningSet(DefaultStructure):
 
-    def __init__(self, id, id_graph, tests, debug_mod):
-        super().__init__(id, id_graph, tests, debug_mod)
+    def __init__(self, id, graph, tests, debug_mod):
+        super().__init__(id, graph, tests, debug_mod)
+        self.batch_nbr = 1
+        self.learning_algorithm = ""
+
+    def run(self):
+        """
+            Method to initialize the learning method approach used.
+        """
+        self.learning_algorithm = self.setLearningAlgorithm()
+
+    def setLearningAlgorithm(self):
+        """
+            Method to set the learning algorithm used for this part.
+            learning_algorithm : A string which represents the learning algorithm used (dichotomic, min_and_max, tag_each_usefull_edge,...)
+        """
+        current_dir = os.getcwd()
+        if current_dir[-1] != '/':
+            current_dir += '/'
+
+        learning_algorithms_available = os.listdir("{0}{1}{2}{3}{4}".format(current_dir, libs_dir, learning_and_testing_dir, learning_dir, learning_algo_dir))
+
+        print("List of learning algorithms:")
+
+        for learning_algorithm in learning_algorithms_available:
+            if not learning_algorithm in not_usefull_files:
+                print("\t{} : {}".format(learning_algorithms_available.index(learning_algorithm), learning_algorithm.split("_approach")[0]))
+
+        print("\n")
+
+        user_choice = int(input("Your choice [0, 1, ...]: "))
+
+        if not user_choice < len(learning_algorithms_available):
+            print("Invalid...")
+            sys.exit()
+
+        return learning_algorithms_available[user_choice].split(".py")[0]
+
+    def makeSomeLearning(self):
+        """
+            Method to return the weights matrix, computed by the learning algorithm function choosen
+        """
+        f = learning_algorithm_functions[self.learning_algorithm]
+
+        return f(self.graph, self.batch_nbr)
