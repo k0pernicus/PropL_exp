c22795758b9c8eebce8429c1bc0e4c8989e2e611
k0pernicus <antonin.carette@gmail.com>
2015-07-17 10:48:48 +0200

    libs/learning_and_testing/DefaultStructure.py : Took the graph object instead of the graph id

--- libs/learning_and_testing_lib/DefaultStructure.py
+++ libs/learning_and_testing_lib/DefaultStructure.py
@@ -2,14 +2,14 @@ class DefaultStructure(object):
     """
         Default structure of the object to get testing and learning data set.
         id : The id of the data set.
-        id_graph : The id of the graph which is the origin of the data set
+        graph : The graph which is the origin of the data set
         tests : Array of tests - a test is represents by a dictionary {"source" : node_source, "targets" : [node_target1, node_target2, ...]}
         debug_mod : A boolean to know if the graph is on debugging mod or not
     """
 
-    def __init__(self, id, id_graph, tests, debug_mod):
+    def __init__(self, id, graph, tests, debug_mod):
         self.id = id
-        self.id_graph = id_graph
+        self.graph = graph
         self.tests = tests
         self.debug_mod = debug_mod
 
