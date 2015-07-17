fc6913fd57fa623f51d76ce311da7e0c6b69b780
k0pernicus <antonin.carette@gmail.com>
2015-07-17 15:48:10 +0200

    Clibs/learning_and_testing_lib/testing/testing_algorithms/test_each_edge_approach.py : Usefull developer documentation C

--- libs/learning_and_testing_lib/testing/testing_algorithms/test_each_edge_approach.py
+++ libs/learning_and_testing_lib/testing/testing_algorithms/test_each_edge_approach.py
@@ -1,3 +1,10 @@
-def test_each_edge_approach():
+def test_each_edge_approach(graph, weights_matrix, tests):
+    """
+        Function to test, on testing examples, the weights matrix.
+        Random probability (between 0 and 1) for each edge.
+        graph : The graph to compute tests with weights_matrix
+        weights_matrix : The weights matrix of usefull edges in the graph
+        tests : Tests made by a learning algorithm (see Learning object)
+    """
 
     pass
