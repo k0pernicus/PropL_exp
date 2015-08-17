4f8fe5d1f70eb80832d242916d66aa5a2d53bc10
k0pernicus <antonin.carette@gmail.com>
2015-08-17 09:52:47 +0200

    tests_script.py : Modification of the initial script

--- tests_script.py
+++ tests_script.py
@@ -1,21 +1,54 @@
 from libs.graph_generation.graph_generation_algorithms.MuscoGraph import MuscoGraph
 from libs.graph_generation.graph_generation_algorithms.BarabasiGraph import BarabasiGraph
 
-if __name__ == "__main__":
+from time import sleep
+
+def musco_script():
+    """
+        Script to create and do some tests on Musco graphs
+    """
 
     musco_graph = MuscoGraph("musco", 25, 37)
     musco_graph.run()
 
-    learning_set, testing_set = musco_graph.generateExamples()
-    learning_set.run()
+    learning_set_m, testing_set_m = musco_graph.generateExamples()
+    learning_set_m.run()
 
-    print("Set : {}\n".format(learning_set.tests))
+    print("Set : {}\n".format(learning_set_m.tests))
 
-    weights_matrix = learning_set.makeSomeLearning()
+    weights_matrix_m = learning_set_m.makeSomeLearning()
 
-    print("{0}\n".format(weights_matrix))
+    print("{0}\n".format(weights_matrix_m))
 
-    testing_set.run()
+    testing_set_m.run()
 
-    print("Weights matrix: {}".format(testing_set.makeSomeTesting(weights_matrix)))
-    print("Test matrix: {}".format(testing_set.tests))
+    print("Weights matrix: {}".format(testing_set_m.makeSomeTesting(weights_matrix_m)))
+    print("Test matrix: {}".format(testing_set_m.tests))
+
+def barabasi_script():
+    """
+        Script to create and do some tests on Barabasi graphs
+    """
+
+    barabasi_graph = BarabasiGraph("musco", 25, 5, 37)
+    barabasi_graph.run()
+
+    learning_set_b, testing_set_b = barabasi_graph.generateExamples()
+    learning_set_b.run()
+
+    print("Set : {}\n".format(learning_set_b.tests))
+
+    weights_matrix_b = learning_set_b.makeSomeLearning()
+
+    print("{0}\n".format(weights_matrix_b))
+
+    testing_set_b.run()
+
+    print("Weights matrix: {}".format(testing_set_b.makeSomeTesting(weights_matrix_b)))
+    print("Test matrix: {}".format(testing_set_b.tests))
+
+if __name__ == "__main__":
+
+    musco_script()
+    sleep(5)
+    barabasi_script()
